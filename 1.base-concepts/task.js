bd75d836db5d4e4a034aa5c89ccf9ae57bbae575
Oulegich <oulegich@mail.ru>
2024-03-21 18:59:32 +0300

    fist HW ready

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+  } else if (d === 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let p = percent / 100 / 12;
+  let S = amount - contribution;
+  let payment = S * (p + p / ((1 + p) ** countMonths - 1));
+  let totalAmount = payment * countMonths;
+  return +totalAmount.toFixed(2);
 }
